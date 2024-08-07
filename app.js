f729d389467c055667531e56b6da25615f7ddf65
MuhammadShahbazahmed315 <muhammadshahbzkhan315@gmail.com>
2024-08-07 11:13:56 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+document.write("My name is Muhammad Shahbaz ahmed");
+document.write(" My father name is Rasheed Ahmed<br> I was lived in Labour Square");
+console.log("My name is Muhammad Shahbaz ahmed");
+var studentName = "Shahbaz ahmed";
+var studentAge = 17;
+var studentGrade = "A";
+var studentEmail = "shabigaming315@gmail.com";
+
+alert("Name: " + studentName);
+alert("Age: " + studentAge);
+alert("Grade: " + studentGrade);
+alert("Email: " + studentEmail);
+var birthYear = 2006;
+document.write
